b6fc2274141f76508e4ef27119e153c55f007992
joyun <choyoon0811@gmail.com>
2024-11-06 22:57:49 +0900

    Pixels
    고전게임처럼 보이도록 픽셀 효과를 주었다.

---
+++ Assets/Pixel.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+[ExecuteInEditMode]
+
+public class Pixel : MonoBehaviour
+{
+    [Range(1 , 100)] public int pixelate;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        source.filterMode = FilterMode.Point;
+        RenderTexture resultTexture = RenderTexture.GetTemporary(source.width / pixelate, source.height / pixelate, 0, source.format);
+        resultTexture.filterMode = FilterMode.Point;
+        Graphics.Blit(source, resultTexture);
+        Graphics.Blit(resultTexture, destination);
+        RenderTexture.ReleaseTemporary(resultTexture);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
