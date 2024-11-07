9f797f467b6466619fbc9cf28aa491a78480372d
joyun <choyoon0811@gmail.com>
2024-11-07 20:08:52 +0900

    Scene
    상자로 가면 씬 전환

--- Assets/script/Box.cs
+++ Assets/script/Box.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Box : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("플레이어와 충돌했습니다!");
+            SceneManager.LoadScene("jumpSqure");
         }
     }
-
-
 }
