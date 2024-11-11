6481572e349a3aef6413f20d4fdc8422699f1a9d
joyun <choyoon0811@gmail.com>
2024-11-11 11:35:53 +0900

    jumpSqureSceneAdd
    처음 점프스퀘어 장면 추가

---
+++ Assets/script/JumpSqure.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JumpSqure : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(scene),2f);
+    }
+
+    void scene()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene 1");
+    }
+}
