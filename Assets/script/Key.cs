dfaec88db7425025c16d23a4fd8b858cbc2c219a
joyun <choyoon0811@gmail.com>
2024-11-14 19:28:34 +0900

    add
    열쇠 추가, 열쇠를 먹어야 상자가 열리게 만들 예정

--- Assets/script/Key.cs
+++ Assets/script/Key.cs
@@ -1,17 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class Box : MonoBehaviour
+public class Key : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("플레이어와 충돌했습니다!");
-            SceneManager.LoadScene("jumpSqure");
+            Destroy(GameObject.FindGameObjectWithTag("Key"));
         }
     }
 }
